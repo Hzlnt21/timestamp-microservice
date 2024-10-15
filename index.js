e9288b906e35117c8279d7a2cc670bf5df54deb7
Hzlnt21 <ivannovicbachtiar2@gmail.com>
2024-10-15 19:10:29 +0700

    Initial commit

--- index.js
+++ index.js
@@ -18,13 +18,33 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// API endpoint untuk Timestamp Microservice
+app.get('/api/:date?', (req, res) => {
+    let dateString = req.params.date;
+    let date;
 
+    if (!dateString) {
+        date = new Date();
+    } else if (!isNaN(dateString)) {
+        date = new Date(+dateString);
+    } else {
+        date = new Date(dateString);
+    }
+
+    if (date.toString() === 'Invalid Date') {
+        return res.json({ error: 'Invalid Date' });
+    }
+
+    res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+    });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
